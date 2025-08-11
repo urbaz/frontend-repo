66093aaea09901378d69024389d9da19c72163b5
urbaz <83726684+urbaz@users.noreply.github.com>
2025-08-11 05:36:52 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://backend-repo-xfe6.onrender.com';
 
 // Goats
 export const getGoats = () => axios.get(`${API_BASE_URL}/goats`);
